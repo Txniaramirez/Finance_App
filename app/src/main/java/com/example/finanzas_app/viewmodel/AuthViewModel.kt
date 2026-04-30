03dd429e58518c02cac498f40fc7e10796b01a4f
Txniaramirez <taniaramirezgomez@gmail.com>
2026-04-30 17:41:06 -0500

    feat(viewmodel): the AuthViewModel class has been created; it needs to be finished

---
+++ app/src/main/java/com/example/finanzas_app/viewmodel/AuthViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.finanzas_app.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finanzas_app.data.database.AppDatabase
+import com.example.finanzas_app.data.model.User
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.security.MessageDigest
+
+/**
+ * AuthViewModel - ViewModel que maneja la autenticación de usuarios
+ **/
+
+class AuthViewModel (application: Application) : AndroidViewModel(application){
+    private val userDao = AppDatabase.getDatabase(application).userDao()
+    private val _currentUser = MutableStateFlow<User?>(null)
+    val currentUser : StateFlow<User?> = _currentUser
+
+    private val _authState = MutableStateFlow<AuthState>(AuthState.Idle)
+    val authState: StateFlow<AuthState> = _authState
+
+    fun login(email: String, password: String){
+        viewModelScope.launch {
+            _authState.value
+
+            try {
+                val user = userDao.getUserByEmail(email)
+                if(user != null && user.passwordHash == hashPassword(password)){
+                    /* Login Exitoso */
+                    _currentUser.value = user
+                    _authState.value = AuthState.Success
+                }else{
+                    _authState.value = AuthState.Error("Email o contraseña incorrecta")
+                }
+            }catch (e: Exception){
+                _authState.value = AuthState.Error("Error al iniciar sesión: ${e.message}")
+            }
+        }
+    }
+
+    private fun hashPassword(password: String): String{
+        val bytes = MessageDigest.getInstance("SHA-256").digest(password.toByteArray())
+        return  bytes.joinToString(" "){"%02x".format(it)}
+    }
+
+    sealed class AuthState{
+        object Idle: AuthState()
+        object Loading: AuthState()
+        object Success: AuthState()
+        data class Error(val message: String): AuthState()
+    }
+}
